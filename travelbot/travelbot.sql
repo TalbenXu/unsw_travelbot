62a783c3ad60e2f432423ea660d4ddf5169873e0
Jeremy <sunhj07@163.com>
2020-03-21 18:38:02 +1100

    travel bot sql file 2.0, for users and location

---
+++ travelbot/travelbot.sql
@@ -0,0 +1,116 @@
+-- COMP9900 20T1
+-- Schema for the Travel site
+--
+---- Written by:
+--    Name:  << Haojun SUN >>
+--    Student ID:  << Z5174534 >>
+--    Date:  09/03/2020
+
+drop database if exists `travelbot`;
+create database `travelbot` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+use travelbot;
+-- Tables
+
+create table Users (
+    user_id             int UNSIGNED AUTO_INCREMENT,
+    family_name         varchar(50),
+    given_name          varchar(50),
+    displayed_name      varchar(50),
+    email_address       varchar(100),
+    password            varchar(50),
+    account_balance     int(8),
+    primary key (user_id)
+)
+COMMENT='users information';
+
+create table Photos (
+    photo_id            int UNSIGNED AUTO_INCREMENT,
+    date_uploaded       timestamp,
+    title               varchar(50),
+    description         text default NULL,
+    file_size           integer,
+    onws_location_id    integer UNSIGNED,
+    primary key (photo_id)
+)
+COMMENT='photo of the location';
+
+create table Locations (
+    location_id         INT UNSIGNED AUTO_INCREMENT,
+    location_name       varchar(50),
+    portrait            int UNSIGNED,
+    primary key (location_id),
+    foreign key (portrait)  references Photos(photo_id)
+)
+COMMENT='location information';
+
+-- add foreign key from photos to locations
+alter table Photos add constraint FK_constraint
+    foreign key (onws_location_id) references Locations(location_id);
+
+create table Products (
+    product_id         INT UNSIGNED AUTO_INCREMENT,
+    product_name       varchar(50),
+    capacity           integer,
+    stock              integer,
+    price              integer,
+    location_id        integer UNSIGNED,
+    primary key (product_id),
+    foreign key (location_id)  references Locations(location_id)
+)
+COMMENT='production information';
+
+create table Payments (
+    payment_id          INT UNSIGNED AUTO_INCREMENT,
+    method_name         varchar(50),
+    amount              integer,
+    payment_date        timestamp,
+    primary key (payment_id)
+)
+COMMENT='payment methods';
+
+create table Orders (
+    order_id            INT UNSIGNED AUTO_INCREMENT,
+    quantity            varchar(50),
+    order_date          timestamp not NULL default current_timestamp,
+    use_date            date,
+    product_id          integer UNSIGNED,
+    payment_id          integer UNSIGNED,
+    primary key (order_id),
+    foreign key (product_id)  references Products(product_id),
+    foreign key (payment_id)  references Payments(payment_id)
+)
+COMMENT='users order information';
+
+create table Upload_Photos (
+    up_photo_id         INT UNSIGNED AUTO_INCREMENT,
+    relate_location_id  integer UNSIGNED,
+    upload_date         timestamp,
+    user_id             integer UNSIGNED,
+    primary key (up_photo_id),
+    foreign key (relate_location_id)  references Locations(location_id),
+    foreign key (user_id)  references Users(user_id)
+)
+COMMENT='users upload photos recognition';
+
+-- relation table
+
+create table Users_Order_History (
+    user_id             integer UNSIGNED,
+    order_id            integer UNSIGNED,
+    primary key (user_id, order_id),
+    foreign key (user_id) references Users(user_id),
+    foreign key (order_id) references Orders(order_id)
+)
+COMMENT='show users many/zero orders';
+
+create table Users_rate_Chatting (
+    user_id             integer UNSIGNED,
+    date_rated          timestamp,
+    rating              enum('one','two','three','four','five'),
+    primary key (user_id),
+    foreign key (user_id) references Users(user_id)
+)
+COMMENT='users rate after chatting';
+
+
